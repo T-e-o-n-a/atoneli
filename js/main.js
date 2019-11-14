cc36532c9647a9eb21e146d26056a73bab795765
T-e-o-n-a <57756603+T-e-o-n-a@users.noreply.github.com>
2019-11-14 16:26:24 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+﻿var swiper1 = new Swiper('.s1', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  effect: 'fade',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  speed: 2000,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  }
+});
+
+
+var swiper2 = new Swiper('.s2', {
+  
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+// var x = document.querySelector("")
